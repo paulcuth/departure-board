1c8b36a1b821ad29a3686e9267a76dae6ccf3b9b
Paul Cuthbertson <paul.cuthbertson@gamesys.co.uk>
2011-07-26 15:18:45 +0100

    Styling added. Still need the animation.

--- js/departure-board.js
+++ js/departure-board.js
@@ -20,7 +20,7 @@ var DepartureBoard = function (element, options) {
 };
 
 
-DepartureBoard.LETTERS = " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,'()&!?+-";
+DepartureBoard.LETTERS = " ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,'()&!?+-";
 
 
 
@@ -32,7 +32,7 @@ DepartureBoard.prototype.spin = function () {
 		(function (i) {
 			window.setTimeout (function () {
 				me._letters[i].spin ();
-			}, 50 * i + Math.random () * 40);
+			}, 20 * i + Math.random () * 400);
 		})(i);
 	}	
 };
@@ -41,6 +41,8 @@ DepartureBoard.prototype.spin = function () {
 
 
 DepartureBoard.prototype.setValue = function (value) {
+	value = value.toUpperCase ();
+	
 	for (var i = 0, l = this._letters.length; i < l; i++) {
 		var letterValue = value.substr (i, 1) || '';
 		this._letters[i].setValue (letterValue);
@@ -57,6 +59,20 @@ DepartureBoard.prototype.setValue = function (value) {
 DepartureBoard.Letter = function () {	
 	this._element = document.createElement ('span');
 	this._element.className = 'letter';
+
+	// this._top = document.createElement ('span');
+	// this._top.className = 'letter top';
+	// this._element.appendChild (this._top);
+	// 
+	// this._fold = document.createElement ('span');
+	// this._fold.className = 'fold';
+	// this._element.appendChild (this._fold);
+	// 
+	// this._flap = document.createElement ('span');
+	// this._flap.className = 'letter flap';
+	// this._fold.appendChild (this._flap);
+	
+	
 	this._index = 0;
 	this._interval = null;
 	this._stopAt = null;
@@ -72,7 +88,9 @@ DepartureBoard.Letter.prototype.getElement = function () {
 
 
 
-DepartureBoard.Letter.prototype.spin = function () {
+DepartureBoard.Letter.prototype.spin = function (clear) {
+	if (clear !== false) this._stopAt = null;
+	
 	var me = this;	
 	this._interval = window.setInterval (function () { me._tick (); }, 40);
 };
@@ -84,17 +102,29 @@ DepartureBoard.Letter.prototype.setValue = function (value) {
 	this._stopAt = DepartureBoard.LETTERS.indexOf (value);
 	if (this._stopAt < 0) this._stopAt = 0;
 
-	if (!this._interval && this._index != this._stopAt) this.spin ();
+	if (!this._interval && this._index != this._stopAt) this.spin (false);
 };
 
 
 
 
 DepartureBoard.Letter.prototype._tick = function () {
+	var me = this;
+
 	this._index = (this._index + 1) % DepartureBoard.LETTERS.length;
 	this._element.innerHTML = DepartureBoard.LETTERS.charAt (this._index);
 
-	if (this._index == this._stopAt) {
+	// this._flap.innerHTML = this._element.innerHTML;
+	// this._top.innerHTML = DepartureBoard.LETTERS.charAt (this._index);
+	// 
+	// this._flap.style.transform = 'scaleX (0)'
+	// 
+	// window.setTimeout (function () {
+	// 	me._flap.style
+	// }, 10);
+	// 
+
+	if (this._index === this._stopAt) {
 		clearInterval (this._interval);
 		delete this._interval;
 	}
